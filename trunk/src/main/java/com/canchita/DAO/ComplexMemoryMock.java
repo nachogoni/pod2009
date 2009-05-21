86ba3c81a76c80d7a66a0e7a74f9460cba473384
martin.palombo <martin.palombo@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-05-21 22:19:50 +0000

    Place Builder y UrlMapper

--- trunk/src/main/java/com/canchita/DAO/ComplexMemoryMock.java
+++ trunk/src/main/java/com/canchita/DAO/ComplexMemoryMock.java
@@ -15,64 +15,55 @@ import com.canchita.model.location.Place;
 
 public class ComplexMemoryMock implements ComplexDAO {
 
-	
-	private static Map<Long,Complex> complexMocks = new HashMap<Long, Complex>();
-		
+	private static Map<Long, Complex> complexMocks = new HashMap<Long, Complex>();
+
 	static {
-		//Initialize an element for mocking purposes
+		// Initialize an element for mocking purposes
 		Complex aComplex = new Complex("Lo de Tincho");
-		
-		ArrayList<String> tels = new ArrayList<String>();
-		
+
 		Calendar titos_horarios = new Calendar();
 		ScoreSystem titos_scores = new ScoreSystem();
 		Expiration titos_expiran = new Expiration();
-		
-		Place titos_place = new Place("Madero 339", tels ,"1233", "Pto Madero", "Buenos Aires",
-				"CABA", "Argentina", "-34.030303", "+54.3434334");
-		titos_place.addTelephone("4444-5555");
-		titos_place.addTelephone("5555-5555");
-		
-		aComplex.setPlace(titos_place);
+
+		Place place = new Place.Builder("Madero 339", "Puerto Madero").town(
+				"CABA").state("Buenos Aires").country("Argentina").latitude(
+				"-34.030303").longitude("-58.3665").telephone("4343-4334")
+				.telephone("5555-5555").build();
+
+		aComplex.setPlace(place);
 		aComplex.setDescription("El complejo mas divertido");
 		aComplex.setTimeTable(titos_horarios);
 		aComplex.setScoreSystem(titos_scores);
-	//	aComplex.setFields(fields);
+		// aComplex.setFields(fields);
 		aComplex.setExpiration(titos_expiran);
 		aComplex.setId(1L);
-		
+
 		ComplexMemoryMock.complexMocks.put(1L, aComplex);
 	}
-	
-	public ComplexMemoryMock(){
-		
+
+	public ComplexMemoryMock() {
+
 	}
-	
-	
+
 	public void delete(Complex complex) {
 		ComplexMemoryMock.complexMocks.remove(complex.getId());
 	}
 
-	
 	public Collection<Complex> getAll() {
 		return ComplexMemoryMock.complexMocks.values();
 	}
 
-	
 	public Complex getById(Long id) {
 		return ComplexMemoryMock.complexMocks.get(id);
 	}
-
 
 	public List<Complex> getFiltered(String name, Locatable location) {
 		return null;
 	}
 
-
 	public void save(Complex complex) {
 		ComplexMemoryMock.complexMocks.put(complex.getId(), complex);
 	}
-
 
 	public void update(Complex complex) {
 		save(complex);
