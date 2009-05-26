611a216784860c471d3f5273f97b4a8e337b0529
pabloks <pabloks@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-05-26 10:12:44 +0000

    Formulario de AddComplex ready!

--- trunk/src/main/java/com/canchita/controller/complex/FormAddComplex.java
+++ trunk/src/main/java/com/canchita/controller/complex/FormAddComplex.java
@@ -12,14 +12,11 @@ public class FormAddComplex extends FormHandler {
 
 	public FormAddComplex() {
 		super();
-		ArrayList<String> elements = new ArrayList<String>();
+		ArrayList<String> scomplejo = new ArrayList<String>();
+		ArrayList<String> subicacion = new ArrayList<String>();
+		ArrayList<String> spuntos = new ArrayList<String>();
 
-		/* armo el decorador */
-		Decorator deco = new Decorator();
-		deco.setFieldset("Complejo");
-		
 		this.setName("Complejo")
-	  		.setFormDecorator(deco)
 	  		.setMethod("post");
 			      
 		this.addElement(new FormElementInput("text","name")
@@ -29,56 +26,92 @@ public class FormAddComplex extends FormHandler {
 
 		this.addElement(new FormElementInput("text","description")
 			.setLabel("Descripción")
-			.setRequired(true));
+			.setRequired(false));
 		
-		this.addElement(new FormElementSelect("country")
-						.setLabel("selectlabel")
-						.addValue("sarasa", "sarasa"));
-		
-		this.addElement(new FormElementInclude("cmbPaises","/home/pabloks/countries_es.html")
-						.setLabel("Pais"));
+		this.addElement(new FormElementInput("text","address")
+			.setLabel("Direccion")
+			.setRequired(true)
+			.addValidator("IsAlphaNum"));
 
-		elements.add("name");
-		elements.add("cmbPaises");
-		this.addDisplayGroup(elements, "posta");
+		this.addElement(new FormElementInput("text","neighbourhood")
+			.setLabel("Barrio")
+			.setRequired(true)
+			.addValidator("IsAlphaNum"));
+
+		this.addElement(new FormElementInput("text","town")
+			.setLabel("Ciudad")
+			.setRequired(true)
+			.addValidator("IsAlphaNum"));
 		
-		elements.add("description");
-		this.addDisplayGroup(elements, "posta2");
-		elements.add("country");
-		this.addDisplayGroup(elements, "posta3");
+		this.addElement(new FormElementInput("text","state")
+			.setLabel("Provincia")
+			.setRequired(true)
+			.addValidator("IsAlphaNum"));
+
+		this.addElement(new FormElementInput("text","zipcode")
+			.setLabel("Codigo Postal")
+			.setRequired(true)
+			.addValidator("IsAlphaNum"));
+
+		this.addElement(new FormElementInput("text","latitude")
+			.setLabel("Latitud"));
+
+		this.addElement(new FormElementInput("text","longitude")
+			.setLabel("Longitud"));
+
+		this.addElement(new FormElementInput("text","telephone")
+			.setLabel("Telefono"));
+
+		this.addElement(new FormElementInput("text","country")
+			.setLabel("Pais"));
 		
-		/*
-		this.addElement(new FormElement("text","name")
-						.setLabel("Direccion")
-						.setRequired(true)
-						.addValidator("IsAlphaNum"));
+		this.addElement(new FormElementInput("text","booking")
+			.setLabel("Reservar")
+			.setRequired(true)
+			.addValidator("IsNumeric"));
 		
-		this.addElement(new FormElement("text","name")
-						.setLabel("Barrio")
-						.setRequired(true)
-						.addValidator("IsAlphaNum"));
+		this.addElement(new FormElementInput("text","deposit")
+			.setLabel("Señar")
+			.setRequired(true)
+			.addValidator("IsNumeric"));
 		
-		this.addElement(new FormElement("text","Ciudad")
-						.setLabel("Nombre")
-						.setRequired(true)
-						.addValidator("IsAlphaNum"));
+		this.addElement(new FormElementInput("text","pay")
+			.setLabel("Pagar")
+			.setRequired(true)
+			.addValidator("IsNumeric"));
 		
-		this.addElement(new FormElement("text","name")
-						.setLabel("Codigo Postal")
-						.setRequired(true)
-						.addValidator("IsAlphaNum"));
+		this.addElement(new FormElementInput("text","downBooking")
+			.setLabel("Cancelar Reserva")
+			.setRequired(true)
+			.addValidator("IsNumeric"));
 		
-		this.addElement(new FormElement("text","name")
-						.setLabel("Latitud"));
+		this.addElement(new FormElementInput("text","downDeposit")
+			.setLabel("Cancelar Deposito")
+			.setRequired(true)
+			.addValidator("IsNumeric"));
+
+		/* agrego los grupos */
+		scomplejo.add("name");
+		scomplejo.add("description");
+		this.addDisplayGroup(scomplejo, "Complejo");
 		
-		this.addElement(new FormElement("text","name")
-						.setLabel("Longitud"));
+		subicacion.add("address");
+		subicacion.add("neighbourhood");
+		subicacion.add("town");
+		subicacion.add("state");
+		subicacion.add("zipcode");
+		subicacion.add("latitude");
+		subicacion.add("longitude");
+		subicacion.add("telephone");
+		subicacion.add("country");
+		this.addDisplayGroup(subicacion, "Ubicacion");
 		
-		this.addElement(new FormElement("text","name")
-						.setLabel("Telefono"));
+		spuntos.add("booking");
+		spuntos.add("deposit");
+		spuntos.add("pay");
+		spuntos.add("downBooking");
+		spuntos.add("downDeposit");
+		this.addDisplayGroup(spuntos, "Sistema de Puntos");
 		
-		this.addElement(new FormElement("text","name")
-						.setLabel("Pais"));
-		*/
 	}
 }
--- trunk/src/main/webapp/WEB-INF/views/complex/AddComplexForm.jsp
+++ trunk/src/main/webapp/WEB-INF/views/complex/AddComplexForm.jsp
@@ -11,54 +11,5 @@
 
 <c:out value="${formulario}" escapeXml="false" />
 
-<form action="" method="post">
-
-	<fieldset>
-		<legend>Complejo</legend>
-		<label for="name">* Nombre: </label>
-		<input type="text" name="name" size="30"/>
-		<label for="description">Descripción: </label>
-		<input type="text" name="description" size="30"/>
-	</fieldset>
-
-	<fieldset>
-		<legend>Ubicación</legend>
-		<label for="address">* Direccion: </label>
-		<input type="text" name="address" size="60"/>
-		<label for="neighbourhood">* Barrio: </label>
-		<input type="text" name="neighbourhood" size="30"/>
-		<label for="town">* Ciudad: </label>
-		<input type="text" name="town" size="30"/>
-		<label for="state">* Provincia: </label>
-		<input type="text" name="state" size="30"/>
-		<label for="zipcode">* Código Postal: </label>
-		<input type="text" name="zipcode" size="30"/>
-		<label for="latitude">Latitud: </label>
-		<input type="text" name="latitude" size="30"/>
-		<label for="longitude">Longitud: </label>
-		<input type="text" name="longitude" size="30"/>
-		<label for="telephone">Teléfono: </label>
-		<input type="text" name="telephone" size="30"/>
-		<label for="country">País: </label>
-		<jsp:include page="/WEB-INF/resources/countries_es.html" flush="true"/>
-   	</fieldset>
-
-	<fieldset>
-		<legend>Sistema de Puntos</legend>
-		<label for="address">* Reservar: </label>
-		<input type="text" name="booking" size="30"/>
-		<label for="neighbourhood">* Seniar: </label>
-		<input type="text" name="deposit" size="30"/>
-		<label for="town">* Pagar: </label>
-		<input type="text" name="pay" size="30"/>
-		<label for="state">* Cancelar Reserva: </label>
-		<input type="text" name="downBooking" size="30"/>
-		<label for="zipcode">* Cancelar Deposito: </label>
-		<input type="text" name="downDeposit" size="30"/>
-   	</fieldset>   	
-   	
-	<input type="submit" value="Agregar" class="submit-go">
-</form>
-
 <jsp:include page="/WEB-INF/views/general/footer/footer.jsp"
 	flush="true" />
