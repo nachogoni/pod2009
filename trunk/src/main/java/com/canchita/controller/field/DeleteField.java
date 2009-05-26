2ba6251033aa6526425a043cc778a36dbe7d5083
nachogoni <nachogoni@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-05-26 04:44:39 +0000

    Back de Field hecho

---
+++ trunk/src/main/java/com/canchita/controller/field/DeleteField.java
@@ -0,0 +1,56 @@
+package com.canchita.controller.field;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.canchita.controller.helper.UrlMapper;
+import com.canchita.controller.helper.UrlMapperType;
+import com.canchita.service.ComplexService;
+import com.canchita.service.FieldService;
+
+/**
+ * Servlet implementation class DeleteField
+ */
+public class DeleteField extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteField() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		FieldService delService = new FieldService();
+
+		Long id = null;
+
+		try {
+			id = Long.parseLong((request.getParameter("id")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			delService.deleteField(id);
+			
+			UrlMapper.getInstance().redirectSuccess(this, request, response,
+					UrlMapperType.GET);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
