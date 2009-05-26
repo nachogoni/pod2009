3538a555316c32be304516b974013e600c9bca3f
cabral.martin <cabral.martin@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-05-26 20:13:16 +0000

    Arreglado el 404 de modify y delete cancha

--- trunk/src/main/java/com/canchita/controller/field/DetailedViewField.java
+++ trunk/src/main/java/com/canchita/controller/field/DetailedViewField.java
@@ -1,6 +1,7 @@
 package com.canchita.controller.field;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,46 +10,71 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import com.canchita.controller.helper.ErrorManager;
 import com.canchita.controller.helper.UrlMapper;
 import com.canchita.controller.helper.UrlMapperType;
+import com.canchita.model.field.Field;
+import com.canchita.service.ComplexService;
 import com.canchita.service.FieldService;
 
 /**
- * Servlet implementation class DetaildedViewField
+ * Servlet implementation class DetailedViewComplex
  */
 public class DetailedViewField extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	Logger logger = Logger.getLogger(DetailedViewField.class.getName());
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		logger.debug("GET request");
-		
-		FieldService service = new FieldService();
 
+	Logger logger = Logger.getLogger(DetailedViewField.class.getName());
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DetailedViewField() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		logger.debug("GET request");
+
+		FieldService service = new FieldService();
 		Long id = null;
 
+		ErrorManager error = new ErrorManager();
+		
 		try {
 			id = Long.parseLong((request.getParameter("id")));
 		} catch (Exception e) {
 			logger.error("Error leyendo id");
-			e.printStackTrace();
+			error.add("El identificador debe ser numérico");
 		}
 
 		try {
-
-			logger.debug("Buscando información de la cancha con id " + id);
+			logger.debug("Buscando detalles de la cancha con id " + id);
 			request.setAttribute("field", service.getById(id));
-			UrlMapper.getInstance().forwardSuccess(this, request, response,
-					UrlMapperType.GET);
 
 		} catch (Exception e) {
-			logger.error("Error buscando información de la cancha con id " + id);
-			e.printStackTrace();
+			error.add(e);
+			logger.error("Error buscando detalles de la cancha con id " + id);
 		}
+
+		if( error.size() != 0 ) {
+			
+			request.setAttribute("errorManager", error);
+
+			UrlMapper.getInstance().forwardFailure(this, request, response,
+					UrlMapperType.GET);
+			
+			return;
+		}
+
+		UrlMapper.getInstance().forwardSuccess(this, request, response,
+				UrlMapperType.GET);
 
 	}
 
