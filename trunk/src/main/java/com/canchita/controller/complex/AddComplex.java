a691d839cef3ca9b5e12f84aef38194f0c519015
cabral.martin <cabral.martin@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-05-26 00:03:51 +0000

    Agregado un pequenio manejo de error para las altas de complejo.

--- trunk/src/main/java/com/canchita/controller/complex/AddComplex.java
+++ trunk/src/main/java/com/canchita/controller/complex/AddComplex.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.canchita.service.ComplexService;
 import com.canchita.views.helpers.ComplexForm;
 import com.canchita.views.helpers.FormHandler;
+import com.canchita.controller.helper.ErrorManager;
 import com.canchita.controller.helper.UrlMapper;
 import com.canchita.controller.helper.UrlMapperType;
 
@@ -52,7 +53,8 @@ public class AddComplex extends HttpServlet {
 		
 		//TODO: Migrar a ComplexForm
 		//TODO: Arreglar el manejo de excepcion y redireccionar a pagina de error.
-		try{
+		ErrorManager error = new ErrorManager();
+
 		String name = request.getParameter("name");
 		String description = request.getParameter("description");
 		
@@ -62,27 +64,60 @@ public class AddComplex extends HttpServlet {
 		String neighbourhood = request.getParameter("neighbourhood");
 		String country = request.getParameter("country");
 		String address = request.getParameter("address");
+
+		if (name == null) {
+			error.add("Falta el nombre del Complejo");
+		}	
+		if(address == null){
+			error.add("Falta la direccion del complejo");
+		}
+		if(town == null){
+			error.add("Falta la cuidad donde se encuentra el complejo");
+		}
+		if(state == null){
+			error.add("Falta la provincia/estado donde se encuentra el complejo");
+		}		
 		
-		Integer booking = Integer.parseInt(request.getParameter("booking"));
-		Integer deposit = Integer.parseInt(request.getParameter("deposit"));
-		Integer pay = Integer.parseInt(request.getParameter("pay"));
-		Integer downBooking = Integer.parseInt(request.getParameter("downBooking"));
-		Integer downDeposit = Integer.parseInt(request.getParameter("downDeposit"));
+		Integer booking = null;
+		Integer deposit = null; 
+		Integer pay = null; 
+		Integer downDeposit = null;
+		Integer downBooking = null;
+
+		try{
 		
-		
+			booking = Integer.parseInt(request.getParameter("booking"));
+			deposit = Integer.parseInt(request.getParameter("deposit"));
+			pay = Integer.parseInt(request.getParameter("pay"));
+			downBooking = Integer.parseInt(request.getParameter("downBooking"));
+			downDeposit = Integer.parseInt(request.getParameter("downDeposit"));
+		} catch (NumberFormatException nfe) {
+			error.add("Valores para el sistema de puntos incorrectos");
+		}
+
+		if (error.size() != 0) {
+			this.failure(request, response, error);
+			return;
+		}
+
 		Long id = addService.saveComplex(name, description, address, zipCode, neighbourhood, town, state, country);
 		addService.addScoreSystem(id, booking, deposit, pay, downBooking, downDeposit);
 		
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	
 
 		
 		UrlMapper.getInstance().forwardSuccess(this, request, response, UrlMapperType.POST);
 		
 		
+	}
+	
+	private void failure(HttpServletRequest request,
+			HttpServletResponse response, ErrorManager error)
+			throws ServletException, IOException {
+		request.setAttribute("errorManager", error);
+
+		UrlMapper.getInstance().forwardFailure(this, request, response,
+				UrlMapperType.GET);
+
 	}
 
 }
