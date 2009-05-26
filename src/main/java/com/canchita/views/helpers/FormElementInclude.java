1fe726a823b93f18443ed0955eda8c82f43ebaa1
pabloks <pabloks@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-05-26 09:23:26 +0000

    Testeo y el Include ya levanta de un file y lo muestra en pantalla.

--- src/main/java/com/canchita/views/helpers/FormElementInclude.java
+++ src/main/java/com/canchita/views/helpers/FormElementInclude.java
@@ -1,22 +1,42 @@
 package com.canchita.views.helpers;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 public class FormElementInclude extends FormElement {
 
 	private String path;
-	
-	public FormElementInclude(String apath) {
-		super("include", apath);
+
+	public FormElementInclude(String aName, String apath) {
+		super("include", aName);
 		path = apath;
 	}
-	
-	public FormElementInclude() {
-		super("include", "path");
-	}
-	
+
 	@Override
 	public String toString() {
-		return String.format("<jsp:include page=\"%s\" flush=\"true\"/>",
-				this.path);
+		String strLine, ret = "";
+
+		try {
+			//levanto el archivo y lo mando
+			FileInputStream fstream = new FileInputStream(this.path);
+
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+			while ((strLine = br.readLine()) != null) {
+				ret += strLine;
+			}
+			// Close the input stream
+			in.close();
+
+		} catch (Exception e) {// Catch exception if any
+			//TODO:No se a donde tiene que ir
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		return super.genLabel() + ret;
 	}
 
 	public String getPath() {
@@ -25,6 +45,14 @@ public class FormElementInclude extends FormElement {
 
 	public FormElementInclude setPath(String path) {
 		this.path = path;
+
+		return this;
+	}
+	
+	@Override
+	public FormElementInclude setLabel(String label) {
+		// TODO Auto-generated method stub
+		super.setLabel(label);
 		
 		return this;
 	}
