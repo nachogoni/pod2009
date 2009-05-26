fea1aafd1341f272b83df1c2277ec8ce939a3d62
macarse <macarse@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-05-26 09:32:50 +0000

    Loggearse loggea admines :D

--- src/main/java/com/canchita/controller/admin/AdminHome.java
+++ src/main/java/com/canchita/controller/admin/AdminHome.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
+import com.canchita.controller.complex.AddComplex;
 import com.canchita.controller.helper.UrlMapper;
 import com.canchita.controller.helper.UrlMapperType;
 
@@ -14,6 +17,7 @@ import com.canchita.controller.helper.UrlMapperType;
  */
 public class AdminHome extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	Logger logger = Logger.getLogger(AdminHome.class.getName());
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -25,11 +29,31 @@ public class AdminHome extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO agarrar los datos del admin
 		UrlMapper.getInstance().forwardSuccess(this, request, response,
 				UrlMapperType.GET);
 	}
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
+		String user;
+		String password;
 
+		logger.debug("POST request");
+
+		user = request.getParameter("username");
+		password = request.getParameter("password");
+		if(!user.equals("admin")) {
+			logger.debug(String.format("Login failed using %s:%s combination.",
+					user, password));
+		}
+
+		UrlMapper.getInstance().forwardSuccess(this, request, response,
+				UrlMapperType.POST);
+		
+		logger.debug("Saliendo del controlador");
+	}
 }
