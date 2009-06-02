8d270be6008fb7d876414208efd0b1ee13fd4a1a
mvidal <mvidal@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-02 19:06:48 +0000

    log4j ahora funca para los que tienen tomcat o jetty. Si tienen tomcat busquen canchita.log en su home. Si tienen jetty en el workspace del tp.

--- trunk/src/main/java/com/canchita/controller/GenericServlet.java
+++ trunk/src/main/java/com/canchita/controller/GenericServlet.java
@@ -1,12 +1,15 @@
 package com.canchita.controller;
 
 import java.io.IOException;
+import java.net.URL;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import com.canchita.controller.admin.AdminHome;
 
@@ -15,13 +18,20 @@ import com.canchita.controller.admin.AdminHome;
  */
 public class GenericServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String LOG4J_PROPERTIES = "log4j.properties";
+
 	protected Logger logger;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GenericServlet() {
-        super();
-        this.logger = Logger.getLogger(this.getClass().getName());
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GenericServlet() {
+		super();
+		URL url = Thread.currentThread().getContextClassLoader().getResource(
+				LOG4J_PROPERTIES);
+		PropertyConfigurator.configure(url);
+
+		this.logger = Logger.getLogger(this.getClass().getName());
+	}
 
 }
