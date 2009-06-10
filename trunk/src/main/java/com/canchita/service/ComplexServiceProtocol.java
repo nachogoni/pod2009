ed27392126d44e988dacf6335762ddfcd3101e8a
macarse <macarse@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-10 22:53:40 +0000

    DAO de complejo sin terminar. Parcialmente andando. (maxi)

--- trunk/src/main/java/com/canchita/service/ComplexServiceProtocol.java
+++ trunk/src/main/java/com/canchita/service/ComplexServiceProtocol.java
@@ -2,17 +2,13 @@ package com.canchita.service;
 
 import java.security.InvalidParameterException;
 import java.util.Collection;
-import java.util.List;
 
 import org.joda.time.DateTime;
 
-import com.canchita.model.booking.Expiration;
 import com.canchita.model.complex.Complex;
-import com.canchita.model.complex.ScoreSystem;
 import com.canchita.model.exception.InvalidScheduleException;
 import com.canchita.model.exception.PersistenceException;
 import com.canchita.model.exception.ValidationException;
-import com.canchita.model.field.Field;
 
 public interface ComplexServiceProtocol {
 
@@ -32,18 +28,19 @@ public interface ComplexServiceProtocol {
 			String state, String country) throws PersistenceException;
 
 	public void addScoreSystem(Long id, Integer booking, Integer deposit,
-			Integer pay, Integer downBooking, Integer downDeposit) throws PersistenceException;
+			Integer pay, Integer downBooking, Integer downDeposit)
+			throws PersistenceException;
 
 	public Complex getById(Long id) throws PersistenceException;
- 
-	
+
 	public void addExpiration(Long id, Integer bookingLimit,
 			Integer depositLimit) throws PersistenceException,
 			InvalidParameterException;
-	
+
 	public void addTimeTable(Long id, DateTime startMon, DateTime endMon,
 			DateTime startTues, DateTime endTues, DateTime startWed,
 			DateTime endWed, DateTime startThurs, DateTime endThurs,
 			DateTime startFri, DateTime endFri, DateTime startSat,
-			DateTime endSat, DateTime startSun, DateTime endSun) throws InvalidScheduleException, PersistenceException;
+			DateTime endSat, DateTime startSun, DateTime endSun)
+			throws InvalidScheduleException, PersistenceException;
 }
