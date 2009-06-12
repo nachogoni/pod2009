5cdbb654c4b228aca6bd3185abab5d16901f0bbc
cabral.martin <cabral.martin@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-12 23:36:22 +0000

    Listo el pollo para la M de ABM para cancha/complejo.
    TODO: Testearlo :)
    
    No se pudo testear AM de complejo/cancha dado que el tomcat no me quiere levantar el proyecto (despues lo pruebo desde casa, pero en teoria en el peor de los casos lo unico que no anda es el agregarle un horario)
    
    TODO2: Hacer que si no pongo nada en el horario de cancha, se use automaticamente el del complejo.

--- src/main/java/com/canchita/controller/field/ModifyField.java
+++ src/main/java/com/canchita/controller/field/ModifyField.java
@@ -1,17 +1,24 @@
 package com.canchita.controller.field;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import com.canchita.controller.GenericServlet;
 import com.canchita.controller.helper.ErrorManager;
 import com.canchita.controller.helper.UrlMapper;
 import com.canchita.controller.helper.UrlMapperType;
+import com.canchita.model.exception.ElementExistsException;
 import com.canchita.model.exception.ElementNotExistsException;
+import com.canchita.model.exception.InvalidScheduleException;
 import com.canchita.model.exception.PersistenceException;
 import com.canchita.model.field.FloorType;
 import com.canchita.service.FieldService;
@@ -22,39 +29,41 @@ import com.canchita.views.helpers.form.FormHandler;
  */
 public class ModifyField extends GenericServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private FormHandler formulario;
-    
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ModifyField() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public ModifyField() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 		logger.debug("GET request");
-		
+
 		FieldService service = new FieldService();
 		Long id = null;
 		formulario = new FormField();
-		
+
 		ErrorManager error = new ErrorManager();
-		
+
 		try {
 			id = Long.parseLong((request.getParameter("id")));
 		} catch (Exception e) {
 			error.add("El identificador debe ser un número");
 			logger.error("Error leyendo id");
 		}
-		
+
 		logger.debug("Buscando información del cancha " + id);
-			
+
 		try {
 			formulario = new FormField(service.getById(id));
 		} catch (PersistenceException e) {
@@ -62,17 +71,17 @@ public class ModifyField extends GenericServlet {
 			error.add(e);
 		}
 
-		if( error.size() != 0 ) {
+		if (error.size() != 0) {
 			request.setAttribute("formulario", this.formulario);
-			
+
 			request.setAttribute("errorManager", error);
-			
+
 			UrlMapper.getInstance().forwardFailure(this, request, response,
 					UrlMapperType.GET);
 			return;
 		}
-		
-		/* Form is sent to the view*/
+
+		/* Form is sent to the view */
 		request.setAttribute("formulario", this.formulario);
 
 		UrlMapper.getInstance().forwardSuccess(this, request, response,
@@ -83,88 +92,155 @@ public class ModifyField extends GenericServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		FieldService modifyService = new FieldService();
-		
+
 		Long id = -1L;
 		Long idComplex = -1L;
 		Boolean hasRoof = false;
 		FloorType floor = FloorType.CONCRETE;
 
-		
-		/*Errors from the past are deleted.*/
+		/* Errors from the past are deleted. */
 		this.formulario.unsetErrors();
 
-		/*Load form with request values*/
+		/* Load form with request values */
 		this.formulario.loadValues(request);
-		
-		if (!this.formulario.isValid())
-		{
+
+		if (!this.formulario.isValid()) {
 			logger.debug("Formulario inválido");
 			request.setAttribute("formulario", formulario);
-			UrlMapper.getInstance().forwardFailure(this, request, response, UrlMapperType.POST);
+			UrlMapper.getInstance().forwardFailure(this, request, response,
+					UrlMapperType.POST);
 			return;
 		}
-		
+
 		ErrorManager error = new ErrorManager();
 
-		
-		
 		String name = request.getParameter("name");
 		String description = request.getParameter("description");
-			
-		try{
+
+		try {
 			hasRoof = Boolean.valueOf(request.getParameter("hasRoof"));
 		} catch (Exception nfe) {
 			error.add("Informacion sobre el tipo de techo invalida");
 		}
-		
-		try{
+
+		try {
 			floor = FloorType.valueOf(request.getParameter("floor"));
 		} catch (Exception nfe) {
 			error.add("Informacion sobre el tipo de piso invalida");
 		}
 
-		try{
+		try {
 			id = Long.parseLong(request.getParameter("id"));
 		} catch (NumberFormatException nfe) {
 			error.add("Valores en la cancha incorrectos");
 		}
-		
-
 
 		if (name == null) {
 			error.add("Falta el nombre de la Cancha");
-		}	
+		}
 
+		if (error.size() != 0) {
+			request.setAttribute("errorManager", error);
+			request.setAttribute("formulario", formulario);
+			UrlMapper.getInstance().forwardFailure(this, request, response,
+					UrlMapperType.POST);
+			return;
+		}
+
+		ArrayList<DateTime> schedule = null;
+		String[] daysOfWeek = { "fecha_lunes_inicio", "fecha_lunes_fin",
+				"fecha_martes_inicio", "fecha_martes_fin",
+				"fecha_miercoles_inicio", "fecha_miercoles_fin",
+				"fecha_jueves_inicio", "fecha_jueves_fin",
+				"fecha_viernes_inicio", "fecha_viernes_fin",
+				"fecha_sabado_inicio", "fecha_sabado_fin",
+				"fecha_domingo_inicio", "fecha_domingo_fin" };
+
+		try {
+
+			schedule = new ArrayList<DateTime>();
+
+			DateTimeFormatter parser = DateTimeFormat.forPattern("HH:mm");
+			String aDay = null;
+
+			for (int i = 0; i < daysOfWeek.length; i++) {
+
+				aDay = request.getParameter(daysOfWeek[i]);
+				System.out.println("parsin start hour for " + daysOfWeek[i]
+						+ ": " + aDay);
+				schedule.add(parser.parseDateTime(aDay));
+				aDay = request.getParameter(daysOfWeek[i++]);
+				System.out.println("parsin start hour for " + daysOfWeek[i]
+						+ ": " + aDay);
+				schedule.add(parser.parseDateTime(aDay));
+
+			}
+
+			// TODO Atrapar la excepcion posta (o crearla si no hay nada
+			// adecuado)
+		} catch (NullPointerException e) {
+			error.add("Faltan valores para los horarios");
+		} catch (Exception e) {
+			error.add("Valores para los horarios incorrectos");
+		}
+		try {
+
+			modifyService.addTimeTable(id, schedule.get(0), schedule.get(1),
+					schedule.get(2), schedule.get(3), schedule.get(4), schedule
+							.get(5), schedule.get(6), schedule.get(7), schedule
+							.get(8), schedule.get(9), schedule.get(10),
+					schedule.get(11), schedule.get(12), schedule.get(13));
+			modifyService.updateField(id, name, description, hasRoof, floor);
+			
+			// TODO loguear los errores
+			
+		} catch (ElementExistsException ee) {
+			error.add(ee);
+			request.setAttribute("errorManager", error);
+			request.setAttribute("formulario", formulario);
+			UrlMapper.getInstance().forwardFailure(this, request, response,
+					UrlMapperType.POST);
+			return;
+			
+		} catch (PersistenceException e) {
+			error.add(e);
+			request.setAttribute("errorManager", error);
+			request.setAttribute("formulario", formulario);
+			UrlMapper.getInstance().forwardFailure(this, request, response,
+					UrlMapperType.POST);
+			return;
+			
+		} catch (IllegalArgumentException e) {
+			logger.error("Error modificando el complejo con id " + id);
+			error.add(e);
+
+		} catch (InvalidScheduleException e) {
+			logger.error("Error modificando el complejo con id " + id);
+			error.add(e);
+
+		} catch (Exception e) {
+			logger.error("Error modificando el complejo con id " + id);
+			error.add(e);
+
+		}
 		
 		if (error.size() != 0) {
 			request.setAttribute("errorManager", error);
 			request.setAttribute("formulario", formulario);
-			UrlMapper.getInstance().forwardFailure(this, request, response, UrlMapperType.POST);
+			UrlMapper.getInstance().forwardFailure(this, request, response,
+					UrlMapperType.POST);
 			return;
 		}
 
-		try {
-			modifyService.updateField(id, name, description, hasRoof, floor);
-		} catch (ElementNotExistsException e) {
-			error.add(e);
-			request.setAttribute("errorManager", error);
-			request.setAttribute("formulario", formulario);
-			UrlMapper.getInstance().forwardFailure(this, request, response, UrlMapperType.POST);
-			return;
-		}catch (PersistenceException e) {
-			error.add(e);
-			request.setAttribute("errorManager", error);
-			request.setAttribute("formulario", formulario);
-			UrlMapper.getInstance().forwardFailure(this, request, response, UrlMapperType.POST);
-			return;
-		}
+		UrlMapper.getInstance().redirectSuccess(this, request, response,
+				UrlMapperType.POST);
 
-		UrlMapper.getInstance().redirectSuccess(this, request, response, UrlMapperType.POST);
-		
 	}
 
 }
