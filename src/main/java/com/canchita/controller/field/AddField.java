045fa56dc30a30c3c6898086b97a1db32074bb32
macarse <macarse@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-13 02:37:17 +0000

    Agregar cancha funcionando.

--- src/main/java/com/canchita/controller/field/AddField.java
+++ src/main/java/com/canchita/controller/field/AddField.java
@@ -1,23 +1,17 @@
 package com.canchita.controller.field;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-
 import com.canchita.controller.GenericServlet;
 import com.canchita.controller.helper.ErrorManager;
 import com.canchita.controller.helper.UrlMapper;
 import com.canchita.controller.helper.UrlMapperType;
 import com.canchita.model.exception.ElementExistsException;
-import com.canchita.model.exception.InvalidScheduleException;
 import com.canchita.model.exception.PersistenceException;
 import com.canchita.model.field.FloorType;
 import com.canchita.service.FieldService.FieldBuilder;
@@ -28,14 +22,12 @@ import com.canchita.views.helpers.form.FormHandler;
  */
 public class AddField extends GenericServlet {
 	private static final long serialVersionUID = 1L;
-	private FormHandler formulario;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public AddField() {
 		super();
-		formulario = new FormField();
 	}
 
 	/**
@@ -47,6 +39,7 @@ public class AddField extends GenericServlet {
 
 		logger.debug("GET request");
 
+		FormHandler formulario = new FormField();
 		String complexId = request.getParameter("id");
 
 		Long id = null;
@@ -75,7 +68,7 @@ public class AddField extends GenericServlet {
 		formulario.setElementValue("idComplex", id.toString());
 
 		/* Form is sent to the view */
-		request.setAttribute("formulario", this.formulario);
+		request.setAttribute("formulario", formulario);
 
 		UrlMapper.getInstance().forwardSuccess(this, request, response,
 				UrlMapperType.GET);
@@ -90,19 +83,19 @@ public class AddField extends GenericServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
+		FormHandler formulario = new FormField();
 		Long idComplex = null;
 		Boolean hasRoof = null;
 		FloorType floor = null;
+		Float price = null;
+		Long number_of_players = null;
 
 		logger.debug("POST request");
 
-		/* Errors from the past are deleted. */
-		this.formulario.unsetErrors();
-
 		/* Load form with request values */
-		this.formulario.loadValues(request);
+		formulario.loadValues(request);
 
-		if (!this.formulario.isValid()) {
+		if (!formulario.isValid()) {
 			logger.debug("Formulario inválido");
 			request.setAttribute("formulario", formulario);
 			UrlMapper.getInstance().forwardFailure(this, request, response,
@@ -133,6 +126,19 @@ public class AddField extends GenericServlet {
 			error.add("Informacion sobre el tipo de piso invalida");
 		}
 
+		try {
+			price = Float.parseFloat(request.getParameter("price"));
+		} catch (NumberFormatException nfe) {
+			error.add("Error en el precio");
+		}
+
+		try {
+			number_of_players = Long.parseLong(request
+					.getParameter("cantPlayers"));
+		} catch (NumberFormatException nfe) {
+			error.add("Error en cantidad de jugadores");
+		}
+
 		if (name == null) {
 			error.add("Falta el nombre de la Cancha");
 		}
@@ -145,64 +151,14 @@ public class AddField extends GenericServlet {
 			return;
 		}
 
-		ArrayList<DateTime> schedule = null;
-		String[] daysOfWeek = { "fecha_lunes_inicio", "fecha_lunes_fin",
-				"fecha_martes_inicio", "fecha_martes_fin",
-				"fecha_miercoles_inicio", "fecha_miercoles_fin",
-				"fecha_jueves_inicio", "fecha_jueves_fin",
-				"fecha_viernes_inicio", "fecha_viernes_fin",
-				"fecha_sabado_inicio", "fecha_sabado_fin",
-				"fecha_domingo_inicio", "fecha_domingo_fin" };
-
 		try {
+			FieldBuilder.Build(name, description, idComplex, hasRoof, floor,
+					price, number_of_players);
 
-			schedule = new ArrayList<DateTime>();
-
-			DateTimeFormatter parser = DateTimeFormat.forPattern("HH:mm");
-			String aDay = null;
-
-			for (int i = 0; i < daysOfWeek.length; i++) {
-
-				aDay = request.getParameter(daysOfWeek[i]);
-				System.out.println("parsin start hour for " + daysOfWeek[i]
-						+ ": " + aDay);
-				schedule.add(parser.parseDateTime(aDay));
-				aDay = request.getParameter(daysOfWeek[i++]);
-				System.out.println("parsin start hour for " + daysOfWeek[i]
-						+ ": " + aDay);
-				schedule.add(parser.parseDateTime(aDay));
-
-			}
-
-			// TODO Atrapar la excepcion posta (o crearla si no hay nada
-			// adecuado)
-		} catch (NullPointerException e) {
-			error.add("Faltan valores para los horarios");
-		} catch (Exception e) {
-			error.add("Valores para los horarios incorrectos");
-		}
-
-		if (error.size() != 0) {
-			request.setAttribute("error", error);
-			request.setAttribute("formulario", formulario);
-			UrlMapper.getInstance().forwardFailure(this, request, response,
-					UrlMapperType.POST);
-			return;
-		}
-
-		try {
-			FieldBuilder.Build(name, description, idComplex, hasRoof, floor);
-
-	/**		FieldBuilder.addTimeTable(schedule.get(0), schedule.get(1),
-					schedule.get(2), schedule.get(3), schedule.get(4), schedule
-							.get(5), schedule.get(6), schedule.get(7), schedule
-							.get(8), schedule.get(9), schedule.get(10),
-					schedule.get(11), schedule.get(12), schedule.get(13));
-*/
 			FieldBuilder.saveField();
 
 		} catch (ElementExistsException ee) {
-			error.add(ee);
+			error.add("No se puede agregar. La cancha ya existe.");
 			request.setAttribute("formulario", formulario);
 			this.failure(request, response, error);
 			return;
