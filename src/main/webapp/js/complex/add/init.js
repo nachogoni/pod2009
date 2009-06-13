19e871f5b77539b53c034acdf87c45fb3dcafe29
macarse <macarse@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-13 03:52:08 +0000

    Fix tildes en tooltips.

--- src/main/webapp/js/complex/add/init.js
+++ src/main/webapp/js/complex/add/init.js
@@ -1,7 +1,11 @@
 $(document).ready( function() {
 setTooltip('#idNombre','Nombre del Complejo');
 $("#divmultiplenames").click(function(){$("#divname").append("<label for='name'> *Nombre</label><div id='divname'><input id='idNombre' type='text' name='name' value='' ></div>")});
-setTooltip('#idDescripcion','Descripción del Complejo');
-setTooltip('#idDireccion','Dirección del Complejo');
+setTooltip('#idDescripcion','Descripci&oacute;n del Complejo');
+setTooltip('#idDireccion','Direcci&oacute;n del Complejo');
 setTooltip('#idBarrio','Barrio del Complejo');
+setTooltip('#depositLimit', 'Cantidad de d&iacute;as l&iacute;mite que ' +
+		'dura una reserva señada' );
+setTooltip('#bookingLimit', 'Cantidad de d&iacute;as l&iacute;mite que ' +
+		'dura una reserva sin ser pagada');
 });
