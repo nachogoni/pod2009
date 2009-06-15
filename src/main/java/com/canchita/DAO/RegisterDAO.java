76fff5b7af0c47978868f1ee750a454bf47bc11e
martin.palombo <martin.palombo@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-15 11:24:13 +0000

    Fix de registracion, si te registras con un nombre de usuario que estaba esperando para cofirmar te aparecia pagina en blanco. Runtime Exception de mierda

--- src/main/java/com/canchita/DAO/RegisterDAO.java
+++ src/main/java/com/canchita/DAO/RegisterDAO.java
@@ -1,11 +1,12 @@
 package com.canchita.DAO;
 
+import com.canchita.model.exception.ElementExistsException;
 import com.canchita.model.exception.PersistenceException;
 import com.canchita.model.user.Registered;
 
 public interface RegisterDAO {
 
-	void saveHash(String username, String password, String email, String hash);
+	void saveHash(String username, String password, String email, String hash) throws ElementExistsException;
 
 	Registered confirmateHash(String hash) throws PersistenceException;
 
--- src/main/java/com/canchita/DAO/db/RegisterDB.java
+++ src/main/java/com/canchita/DAO/db/RegisterDB.java
@@ -1,5 +1,6 @@
 package com.canchita.DAO.db;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.canchita.DAO.RegisterDAO;
@@ -8,6 +9,7 @@ import com.canchita.DAO.db.builders.RegistrationInfoBuilder;
 import com.canchita.DAO.factory.DAOFactory;
 import com.canchita.DAO.factory.FactoryMethod;
 import com.canchita.DAO.factory.DAOFactory.DAO;
+import com.canchita.model.exception.ElementExistsException;
 import com.canchita.model.exception.PersistenceException;
 import com.canchita.model.user.Registered;
 import com.canchita.model.user.RegistrationInfo;
@@ -27,12 +29,24 @@ public class RegisterDB extends AllDB implements RegisterDAO {
 
 	@Override
 	public void saveHash(String username, String password, String email,
-			String hash) {
+			String hash) throws ElementExistsException {
 
 		String query = "INSERT into REGISTER(\"name\",\"password\",\"email\",\"is_admin\",\"hash\") VALUES(?, ?, ?, ?, ?)";
-		executeUpdate(query, new Object[] { username, password, email, false,
-				hash });
+		
+		try {
+			executeUpdate(query, new Object[] { username, password, email, false,
+					hash });
+		}
+		catch(RuntimeException re) {
+			Throwable sql = re.getCause();
 
+			if (sql instanceof SQLException) {
+				if (sql.getMessage().contains("REGISTER_NAME_UNIQUE")) {
+					throw new ElementExistsException(
+							"Ya existe un usuario con ese nombre");
+				}
+			}
+		}
 	}
 
 	@Override
