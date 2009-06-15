a6718be06021cf7af7f8edb7097551ef6781e03a
macarse <macarse@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-15 16:36:34 +0000

    /admin/email necesita fix. Commit para lombo.

---
+++ src/main/java/com/canchita/controller/admin/Email.java
@@ -0,0 +1,101 @@
+package com.canchita.controller.admin;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.canchita.controller.GenericServlet;
+import com.canchita.controller.helper.ErrorManager;
+import com.canchita.controller.helper.UrlMapper;
+import com.canchita.controller.helper.UrlMapperType;
+import com.canchita.model.exception.UserException;
+import com.canchita.model.user.Registered;
+import com.canchita.service.UserService;
+import com.canchita.service.UserServiceProtocol;
+import com.canchita.views.helpers.form.FormHandler;
+
+/**
+ * Servlet implementation class Profile
+ */
+public class Email extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see GenericServlet#GenericServlet()
+	 */
+	public Email() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Registered user = this.getUser(request);
+		FormHandler form = new FormEmail(user);
+
+		request.setAttribute("formulario", form);
+
+		UrlMapper.getInstance().forwardSuccess(this, request, response,
+				UrlMapperType.GET);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		logger.debug("POST request");
+
+		FormHandler form = new FormEmail();
+
+		/* Load form with request values */
+		form.loadValues(request);
+
+		if (!form.isValid()) {
+			logger.debug("Formulario inválido");
+			request.setAttribute("formulario", form);
+			UrlMapper.getInstance().forwardFailure(this, request, response,
+					UrlMapperType.POST);
+			return;
+		} else {
+			ErrorManager error = new ErrorManager();
+			Registered user = this.getUser(request);
+			String[] emails = null;
+			UserServiceProtocol userService = new UserService();
+
+			// Se actualizan los mails.
+			try {
+				emails = request.getParameterValues("email");
+				userService.updateEmails(user, emails);
+
+			} catch (UserException e) {
+				error.add(e);
+				request.setAttribute("formulario", form);
+				UrlMapper.getInstance().forwardFailure(this, request, response,
+						UrlMapperType.GET);
+				return;
+			}
+
+			Map<String, String> map = new HashMap<String, String>();
+
+			map.put("update", "true");
+
+			UrlMapper.getInstance().redirectSuccess(this, request, response,
+					UrlMapperType.POST, map);
+
+		}
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/user/registered/admin/Email.jsp
@@ -0,0 +1,45 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core"%>
+<%@ taglib prefix="c_rt" uri="http://java.sun.com/jstl/core_rt"%>
+
+<jsp:include page="/WEB-INF/views/general/header/header.jsp"
+	flush="true">
+	<jsp:param name="title" value="Modificar Perfil" />
+</jsp:include>
+
+<h1>Modificar E-mails</h1>
+
+<c:if test="${(param.success != null)}">
+
+	<c:choose>
+		<c:when test="${param.success}">
+			<c:set var="successMsg" scope="page"
+				value="El perfil fue modificado exitosamente" />
+		</c:when>
+		<c:otherwise>
+			<c:set var="successMsg" scope="page"
+				value="Error al modificar el perfil" />
+		</c:otherwise>
+	</c:choose>
+
+	<div class="ui-state-highlight ui-corner-all info"><span
+		class="ui-icon ui-icon-info infoIcon"></span> <span><strong>Información:</strong></span>
+	<span class="block">* <c:out value="${successMsg}" /></span></div>
+</c:if>
+
+<c:if test="${(errorManager != null)}">
+	<div class="ui-state-error ui-corner-all error"><span
+		class="ui-icon ui-icon-alert errorIcon"></span> <span> <strong>Alerta:</strong>
+	</span> <c:forEach items="${errorManager.errors}" var="error">
+		<span class="block">* <c:out value="${error}" /></span>
+	</c:forEach></div>
+</c:if>
+
+<c:out value="${formulario}" escapeXml="false" />
+
+<jsp:include page="/WEB-INF/views/general/js.jsp" flush="true" />
+<script
+	src="<c:out value="${baseURI}" escapeXml="false" />/js/admin/email/init.js"
+	type="text/javascript" /></script>
+
+<jsp:include page="/WEB-INF/views/general/footer/footer.jsp"
+	flush="true" />
