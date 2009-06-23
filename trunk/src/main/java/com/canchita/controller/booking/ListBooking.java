d9fcecf9d9b22587d0ae55112eb30829b5be8973
pabloks <pabloks@9e07c2b4-358c-11de-9d05-31133e6853b1>
2009-06-23 04:14:45 +0000

    Sort de Booking

--- trunk/src/main/java/com/canchita/controller/booking/ListBooking.java
+++ trunk/src/main/java/com/canchita/controller/booking/ListBooking.java
@@ -6,10 +6,16 @@ import com.canchita.controller.helper.UrlMapper;
 import com.canchita.controller.helper.UrlMapperType;
 import com.canchita.model.booking.Booking;
 import com.canchita.model.exception.BookingException;
+import com.canchita.model.field.Field;
 import com.canchita.service.BookingService;
 import com.canchita.service.BookingServiceProtocol;
+import com.canchita.views.helpers.form.Pair;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.Servlet;
@@ -46,7 +52,36 @@ public class ListBooking extends GenericServlet implements Servlet {
 			return;
 		}
 
-		request.setAttribute("bookings", bookings);
+		// ordeno la lista
+		List<Booking> list = new ArrayList<Booking>(bookings);
+		String typesort = "";
+
+		ArrayList<String> parametersSort = new ArrayList<String>();
+		HashMap<String, Pair<Comparator<Booking>, Comparator<Booking>>> comparators = new HashMap<String, Pair<Comparator<Booking>, Comparator<Booking>>>();
+		parametersSort.add("sortUserName");
+		comparators.put("sortUserName", new Pair<Comparator<Booking>, Comparator<Booking>>(Booking.compareUserNames(true),Booking.compareUserNames(false)));
+		parametersSort.add("sortField");
+		comparators.put("sortField", new Pair<Comparator<Booking>, Comparator<Booking>>(Booking.compareFields(true),Booking.compareFields(false)));
+		parametersSort.add("sortComplex");
+		comparators.put("sortComplex", new Pair<Comparator<Booking>, Comparator<Booking>>(Booking.compareComplexs(true),Booking.compareComplexs(false)));
+		parametersSort.add("sortState");
+		comparators.put("sortState", new Pair<Comparator<Booking>, Comparator<Booking>>(Booking.compareStates(true),Booking.compareStates(false)));
+		parametersSort.add("sortCost");
+		comparators.put("sortCost", new Pair<Comparator<Booking>, Comparator<Booking>>(Booking.compareCosts(true),Booking.compareCosts(false)));
+		parametersSort.add("sortPay");
+		comparators.put("sortPay", new Pair<Comparator<Booking>, Comparator<Booking>>(Booking.comparePays(true),Booking.comparePays(false)));
+
+		for (String e : parametersSort) {
+			if ((typesort = request.getParameter(e)) != null) {
+				Collections.sort(list, typesort.equals("ASC") ? comparators.get(e).getFirst() : comparators.get(e).getSecond());
+				request.setAttribute(String.format("%sTypeR", e), typesort.equals("ASC") ? "DESC" : "ASC");
+				request.setAttribute(String.format("%sType", e), typesort);
+			} else {
+				request.setAttribute(String.format("%sTypeR", e), "ASC");
+			}
+		}
+		
+		request.setAttribute("bookings", list);
 		request.setAttribute("bookingsLength", bookings.size());
 		request.setAttribute("showComplex", true);
 		request.setAttribute("showField", true);
